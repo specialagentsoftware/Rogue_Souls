9357c89728e9bb714ce8177149b6ac233d9312b9
Jeremy Bond <bond_jeremy_w@outlook.com>
2024-08-02 23:35:29 -0400

    implement state management

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -9,7 +9,8 @@ extends CharacterBody2D
 @export var idle_animation_speed :float = 1.2
 @onready var asprite : AnimatedSprite2D = $Marker2D/Graphics/AnimatedSprite2D
 @onready var ap :AnimationPlayer = $Marker2D/Graphics/AnimationPlayer
-
+enum states {Attacking, Walking, Idling, Blocking, Dodging}
+@onready var current_state = states.Idling
 
 func get_movement() -> Vector2:
 	var input: Vector2 = Vector2()
@@ -28,27 +29,30 @@ func _physics_process(delta:float) -> void:
 	
 	if direction.x == 0 and direction.y == 0 and (ap.current_animation != "Punch" or ap.current_animation != "Block"):
 		asprite.visible = false
+		current_state = states.Idling
 	elif (ap.current_animation != "Punch" or ap.current_animation != "Block"):
 		asprite.visible = true
-		
+		current_state = states.Walking
 		
 	if Input.is_action_just_pressed("punch"):
 		if ap.current_animation == "Punch":
 			return
 		ap.stop()
 		ap.play("Punch",-1,punch_animation_speed,false)
+		current_state = states.Attacking
 		
 	if Input.is_action_just_pressed("block"):
 		if ap.current_animation == "Block":
 			return
 		ap.stop()
 		ap.play("Block",-1,block_animation_speed,false)
+		current_state = states.Blocking
 	
 	if direction.length() > 0:
 		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
 	else:
 		velocity = velocity.lerp(Vector2.ZERO, friction)
 		
-	print(ap.current_animation)
 	move_and_slide()
+	print(current_state)
 	
